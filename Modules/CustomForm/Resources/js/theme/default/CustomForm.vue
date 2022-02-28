07829260c7f0bc1868e29323cd9b4ecb2b082c94
stanislavPc <stanislav.cozac@g.picanova.com>
2022-02-28 17:58:39 +0200

    updated form validate response

--- Modules/CustomForm/Resources/js/theme/default/CustomForm.vue
+++ Modules/CustomForm/Resources/js/theme/default/CustomForm.vue
@@ -4,61 +4,79 @@
         <form :action="form.action" :method="form.method" @submit.prevent="onSubmit"
               :enctype="form.file ? 'multipart/form-data' : 'application/x-www-form-urlencoded'">
             <div class="row">
-                <div :class="['mb-3', field.block_class || 'col-12']" v-for="(field, field_key) in form.fields"
-                     v-if="fieldInput.includes(field.type)">
-                    <label :for="`field_${field.field_id || field.id}`" class="form-label" v-if="field.label">
-                        {{ field.label }}
-                    </label>
-                    <input :type="field.type" :class="[field.type ==='range' ? 'form-range' : 'form-control', field.field_class]"
-                           v-model="field.value" :placeholder="field.placeholder" :required="isValidate(field)" :name="field.name"
-                           :pattern="isValidate(field, true)" :id="`field_${field.field_id || field.id}`">
+                <div :class="['mb-3', field.block_class || 'col-12']" v-for="(field, field_key) in form.fields">
+                    <template v-if="fieldInput.includes(field.type)">
+                        <label :for="`field_${field.field_id || field.id}`" class="form-label" v-if="field.label">
+                            {{ field.label }}
+                        </label>
+                        <input :type="field.type" :name="field.name" :pattern="isValidate(field, true)"
+                               :class="[field.type ==='range' ? 'form-range' : 'form-control', field.field_class, has(field.name) ? 'is-invalid' : '']"
+                               v-model="field.value" :placeholder="field.placeholder" :required="isValidate(field)"
+                               :id="`field_${field.field_id || field.id}`">
+                        <div class="invalid-feedback" v-if="has(field.name)">
+                            <div v-for="error in get(field.name)">{{ error }}</div>
+                        </div>
+                    </template>
+                    <template v-else-if="field.type === 'file'">
+                        <label :for="`file_${field.field_id || field.id}`" class="form-label" v-if="field.label">
+                            {{ field.label }}
+                        </label>
+                        <input :type="field.type" :name="field.name" @change="addFile($event, field_key)"
+                               :class="['form-control', field.field_class, has(field.name) ? 'is-invalid' : '']"
+                               :required="isValidate(field)" :id="`file_${field.field_id || field.id}`">
+                        <div class="invalid-feedback" v-if="has(field.name)">
+                            <div v-for="error in get(field.name)">{{ error }}</div>
+                        </div>
+                    </template>
+                    <template v-else-if="field.type === 'textarea'">
+                        <label :for="`textarea_${field.field_id || field.id}`" v-if="field.label" class="form-label">
+                            {{ field.label }}
+                        </label>
+                        <textarea :class="['form-control', field.field_class, has(field.name) ? 'is-invalid' : '']"
+                                  v-model="field.value" :placeholder="field.placeholder" :required="isValidate(field)"
+                                  :pattern="isValidate(field, true)" rows="3"
+                                  :id="`textarea_${field.field_id || field.id}`"
+                        ></textarea>
+                        <div class="invalid-feedback" v-if="has(field.name)">
+                            <div v-for="error in get(field.name)">{{ error }}</div>
+                        </div>
+                    </template>
+                    <template v-else-if="fieldRadioCheck.includes(field.type)">
+                        <label v-if="field.label" class="form-label">{{ field.label }}</label>
+                        <br>
+                        <div :class="['form-check form-check-inline', `custom-${field.type}`]"
+                             v-for="option in field.options">
+                            <input :type="field.type" :id="`key_${option.option_id || option.id}`" :value="option.value"
+                                   :class="['form-check-input', option.option_class, has(field.name) ? 'is-invalid' : '']"
+                                   :name="field.name" v-model="field.value" :required="isValidate(field)">
+                            <label class="form-check-label" :for="`key_${option.option_id || option.id}`"
+                                   v-html="option.title"></label>
+                            <div class="invalid-feedback" v-if="has(field.name)">
+                                <div v-for="error in get(field.name)">{{ error }}</div>
+                            </div>
+                        </div>
+                    </template>
+                    <template v-else-if="field.type === 'select'">
+                        <label :for="`select_${field.field_id || field.id}`" v-if="field.label" class="form-label">
+                            {{ field.label }}
+                        </label>
+                        <select :class="['form-select', field.field_class, has(field.name) ? 'is-invalid' : '']"
+                                :id="`select_${field.field_id || field.id}`" v-model="field.value"
+                                :required="isValidate(field)">
+                            <option v-for="option in field.options" :value="option.value"
+                                    :class="[option.option_class ? option.option_class : null]"
+                                    :id="[option.option_id ? option.option_id : null]">
+                                {{ option.title }}
+                            </option>
+                        </select>
+                        <div class="invalid-feedback" v-if="has(field.name)">
+                            <div v-for="error in get(field.name)">{{ error }}</div>
+                        </div>
+                    </template>
                 </div>
-                <div :class="['mb-3', field.block_class || 'col-12']" v-else-if="field.type === 'file'">
-                    <label :for="`file_${field.field_id || field.id}`" class="form-label" v-if="field.label">
-                        {{ field.label }}
-                    </label>
-                    <input :type="field.type" :name="field.name" :class="['form-control', field.field_class]"
-                           :required="isValidate(field)" :id="`file_${field.field_id || field.id}`"
-                           @change="addFile($event, field_key)">
-                </div>
-                <div :class="['mb-3', field.block_class || 'col-12']" v-else-if="field.type === 'textarea'">
-                    <label :for="`textarea_${field.field_id || field.id}`" v-if="field.label" class="form-label">
-                        {{ field.label }}
-                    </label>
-                    <textarea :class="['form-control', field.field_class]" v-model="field.value" :placeholder="field.placeholder"
-                              rows="3" :id="`textarea_${field.field_id || field.id}`" :required="isValidate(field)"
-                              :pattern="isValidate(field, true)"></textarea>
-                </div>
-                <div :class="['mb-3', field.block_class || 'col-12']" v-else-if="fieldRadioCheck.includes(field.type)">
-                    <label v-if="field.label" class="form-label">{{ field.label }}</label>
-                    <br>
-                    <div :class="['form-check form-check-inline', `custom-${field.type}`]" v-for="option in field.options">
-                        <input :type="field.type" :class="['form-check-input', option.option_class]" :name="field.name"
-                               v-model="field.value" :required="isValidate(field)" :id="`key_${option.option_id || option.id}`"
-                               :value="option.value">
-                        <label class="form-check-label" :for="`key_${option.option_id || option.id}`"
-                               v-html="option.title"></label>
-                    </div>
-                </div>
-                <div :class="['mb-3', field.block_class || 'col-12']" v-else-if="field.type === 'select'">
-                    <label :for="`select_${field.field_id || field.id}`" v-if="field.label" class="form-label">
-                        {{ field.label }}
-                    </label>
-                    <select :class="['form-select', field.field_class]" :id="`select_${field.field_id || field.id}`"
-                            v-model="field.value" :required="isValidate(field)">
-                        <option v-for="option in field.options" :value="option.value"
-                                :class="[option.option_class ? option.option_class : null]"
-                                :id="[option.option_id ? option.option_id : null]">
-                            {{ option.title }}
-                        </option>
-                    </select>
-                </div>
-            </div>
-            <div class="alert alert-danger" role="alert" v-if="Object.keys(errorsValidate).length">
-                <div v-for="error in errorsValidate">{{ error[0] }}</div>
             </div>
             <div class="alert alert-success" role="alert" v-if="success">
-                <div>{{ success }}</div>
+                {{ success }}
             </div>
             <div class="text-center my-3">
                 <button :class="{'btn-custom': true, 'btn-loading': loading}" type="submit">
@@ -106,8 +124,9 @@ export default {
     computed: {
         sendFields() {
             let fields = {};
-            for (let f = 0; f < this.form.fields.length; f++)
-                fields[this.form.fields[f].name] = this.form.fields[f].value;
+            this.form.fields.forEach(item => {
+                fields[item.name] = item.value;
+            })
 
             return fields;
         }
@@ -115,8 +134,9 @@ export default {
     async fetch() {
         if (this.source) {
             const {data} = await axios.get(this.source)
-            if (typeof data.data !== "undefined")
+            if (typeof data.data !== "undefined") {
                 this.form = data.data;
+            }
 
             this.trans = data.trans
         }
@@ -156,7 +176,7 @@ export default {
                 method: this.form.method,
                 url: this.form.action,
                 data: {id: this.form.id, fields: this.sendFields}
-            }).then((response) => {
+            }).then(() => {
                 this.success = this.$t('CustomForm.message_sends');
                 for (let objectField of this.form.fields) {
                     if (Array.isArray(objectField.value))
@@ -173,6 +193,15 @@ export default {
                     this.loading = false;
                 })
             });
+        },
+        has(field) {
+            return this.errorsValidate.hasOwnProperty(field)
+        },
+        get(field) {
+            if (this.errorsValidate[field]) {
+                return this.errorsValidate[field]
+            }
+            return [];
         }
     }
 }
