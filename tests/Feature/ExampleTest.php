20ee8cff43eb69e8de527e5368669ea9dc711a4a
fynduck <stasskrim_-92@mail.ru>
2022-03-15 12:14:14 +0200

    updated unit test and added it to actions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/get-app-data');
 
         $response->assertStatus(200);
     }
